7ffc9b716e87254cb1e30c7580d1ee149772e5d9
jojogames632 <jojogames@free.fr>
2021-11-06 18:20:12 +0100

    Add planning

---
+++ src/Controller/PlanningController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PlanningRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PlanningController extends AbstractController
+{
+    /**
+     * @Route("/planning", name="planning")
+     */
+    public function index(PlanningRepository $planningRepository): Response
+    {
+        $days = $planningRepository->findAll();
+
+        return $this->render('planning/index.html.twig', [
+            'days' => $days
+        ]);
+    }
+}
