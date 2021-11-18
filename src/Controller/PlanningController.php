2471f6bfdb6e0688ba1d32a23a26899291ac1453
jojogames632 <jojogames@free.fr>
2021-11-18 12:50:38 +0100

    add clean planning option in planning page

--- src/Controller/PlanningController.php
+++ src/Controller/PlanningController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Planning;
 use App\Form\PlanningType;
 use App\Repository\PlanningRepository;
-use App\Repository\RecipeFoodRepository;
 use App\Repository\ShoppingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -17,7 +16,7 @@ class PlanningController extends AbstractController
     /**
      * @Route("/planning/{planningOwner}", name="planning")
      */
-    public function index(PlanningRepository $planningRepository, ShoppingRepository $shoppingRepository, Request $request, string $planningOwner = 'Christophe')
+    public function index(PlanningRepository $planningRepository, Request $request, string $planningOwner = 'Christophe')
     {
         $owners = $planningRepository->findAllOwners();
         $entityManager = $this->getDoctrine()->getManager();
@@ -55,20 +54,33 @@ class PlanningController extends AbstractController
             return $this->redirectToRoute('planning');
         }
 
+        // clean planning
+        if (isset($_POST['cleanPlanningOf'])) {
+            $owner = htmlspecialchars($_POST['cleanPlanningOf']);
+            $planningRows = $planningRepository->findByOwner($owner);
+            $entityManager = $this->getDoctrine()->getManager();
+
+            foreach ($planningRows as $planningRow) {
+                $planningRow->setMiddayRecipe(null);
+                $planningRow->setMiddayPersons(null);
+                $planningRow->setEveningPersons(null);
+                $planningRow->setEveningRecipe(null);
+                $entityManager->persist($planningRow);
+            }
+
+            $entityManager->flush();
+        }
+
         // delete planning
         if (isset($_POST['deletePlanningOf'])) {
             $owner = htmlspecialchars($_POST['deletePlanningOf']);
             $planningRows = $planningRepository->findBy(['owner' => $owner]);
-            $shoppingRows = $shoppingRepository->findBy(['owner' => $owner]);
 
             // planning
             foreach ($planningRows as $planningRow) {
                 $entityManager->remove($planningRow);
             }
-            // shopping
-            foreach ($shoppingRows as $shoppingRow) {
-                $entityManager->remove($shoppingRow);  
-            }
+
             $entityManager->flush();
 
             return $this->redirect($this->generateUrl('planning', ['planningOwner' => $planningOwner]));
@@ -85,22 +97,18 @@ class PlanningController extends AbstractController
     }
 
     /**
-     * @Route("/delete-recipe/{day}/{when}/{persons<\d+>}{owner}", name="delete_recipe_in_planning")
+     * @Route("/delete-recipe/{day}/{when}/{owner}", name="delete_recipe_in_planning")
      */
-    public function deleteRecipe(string $day, string $when, int $persons, string $owner, PlanningRepository $planningRepository, RecipeFoodRepository $recipeFoodRepository, ShoppingRepository $shoppingRepository)
+    public function deleteRecipe(string $day, string $when, string $owner, PlanningRepository $planningRepository)
     {
-        $recipe = null;
-        // delete in planning ////////////////////////////////////////////////////// phase 1
-
+        // delete in planning
         $planningDay = $planningRepository->findOneByNameAndOwner($day, $owner);
 
         if ($when === 'midi') {
-            $recipe = $planningDay->getMiddayRecipe();
             $planningDay->setMiddayRecipe(null);
             $planningDay->setMiddayPersons(null);
         }
         else {
-            $recipe = $planningDay->getMiddayRecipe();
             $planningDay->setEveningRecipe(null);
             $planningDay->setEveningPersons(null);
         } 
@@ -108,25 +116,6 @@ class PlanningController extends AbstractController
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->persist($planningDay);
         $entityManager->flush();
-
-        // delete in shopping ////////////////////////////////////////////////////// phase 2
-
-        $multiplier = $persons / $recipe->getPersons();
-
-        // for each foods of the recipe
-        $recipeFoods = $recipeFoodRepository->findBy(['recipe' => $recipe]);
-        foreach ($recipeFoods as $recipeFood) {
-            // get the line and substract
-            $shoppingRow = $shoppingRepository->findOneByFoodUnitAndOwner($recipeFood->getFood(), $recipeFood->getUnit(), $owner);
-            $shoppingRow->setQuantity($shoppingRow->getQuantity() - $recipeFood->getQuantity() * $multiplier);
-            $entityManager->persist($shoppingRow);
-            $entityManager->flush();
-            // del if 0 quantity
-            if ($shoppingRow->getQuantity() == 0) {
-                $entityManager->remove($shoppingRow);
-                $entityManager->flush();
-            }
-        }
 
         return $this->redirect($this->generateUrl('planning', [
             'planningOwner' => $owner
